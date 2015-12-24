996054ce890970332762440c8e0537c77e77b576
PSNAppZ <psnappz@gmail.com>
2015-12-24 19:46:08 +0530

    Update 0.1a

--- PSCQuiz/Form1.vb
+++ PSCQuiz/Form1.vb
@@ -1,29 +1,20 @@
 ﻿Imports System.IO
 
 Public Class Form1
+  
     Dim spath As String = Path.GetFullPath(Application.StartupPath)
-    Dim s As String = "C:\Users\User\documents\visual studio 2013\Projects\PSCQuiz\PSCQuiz\Quiz\Quiz.txt"
-    Dim sr As New System.IO.StreamReader(s)
-    Dim reader
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-       
-    End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Registeration.Show()
+    End Sub
 
-        If (Not System.IO.Directory.Exists(Path.Combine(spath, "\Quiz"))) Then
-            System.IO.Directory.CreateDirectory(Path.Combine(spath, "Quiz"))
-            If (Not System.IO.File.Exists(Path.Combine(spath, "\Quiz\Quiz.txt"))) Then
-                System.IO.File.Create(Path.Combine(spath, "\Quiz\Quiz.txt"))
-            End If
-        End If
-
-        Dim s As String = "C:\Users\User\documents\visual studio 2013\Projects\PSCQuiz\PSCQuiz\Quiz\Quiz.txt"
-        MsgBox(spath)
-        TextBox1.Text = sr.ReadLine()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Quiz.Show()
     End Sub
 End Class
+
