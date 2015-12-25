a667df531a205e2c29407bb7f2ee5f8aa24daf13
PSNAppZ <psnappz@gmail.com>
2015-12-25 11:24:50 +0530

    Update v1.1

--- PSCQuiz/Form1.vb
+++ PSCQuiz/Form1.vb
@@ -15,6 +15,7 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         Dashboard.Show()
+        ''Test
     End Sub
 End Class
 
